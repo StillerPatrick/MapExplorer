2f175cf8cb91f5abaef9c8e933c314baa73fb707
stillerpatrick <p.stiller@ebs-z.de>
2019-09-09 17:10:45 +0200

    "first runable version of dataset and train script"

--- train.py
+++ train.py
@@ -3,7 +3,8 @@ import torch
 import horovod.torch as hvd 
 import numpy as np 
 from dataset import Mapdataset
-
+from models.unet import UNet
+from loss.loss import SSIM
 # define command line parameter 
 parser = argparse.ArgumentParser(description="Define the parameter of the training process")
 
@@ -24,4 +25,24 @@ args = parser.parse_args()
 
 trainDataset = Mapdataset(args.basedir, args.gpu)
 trainLoader = torch.utils.data.DataLoader(trainDataset,args.batch_size,args.shuffle,num_workers=2)
+
+model = UNet(3,1)
+
+optimizer = torch.optim.Adam(model.parameters(),lr=1e-3)
+
+ssim = SSIM()
+
+# training loop 
+for epoch in range(args.epochs):
+    for train_x, train_y in trainLoader:
+            optimizer.zero_grad()
+            prediction = model(train_x)
+            loss = -ssim(train_y, prediction)
+            loss.backward()
+            optimizer.step()
+            if epoch % int(args.epoch / 10):
+                print("Loss at Epoch",epoch+1,":",loss.item())
+        
+
+
 
