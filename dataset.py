b263463ba8a6802cd2030a2064d41d4c80532750
stillerpatrick <p.stiller@ebs-z.de>
2019-09-09 18:27:29 +0200

    fix dataloader

--- dataset.py
+++ dataset.py
@@ -4,6 +4,8 @@ import numpy as np
 import torch
 import os 
 import cv2
+from tqdm import tqdm
+
 
 class Mapdataset(Dataset):
 
@@ -11,17 +13,34 @@ class Mapdataset(Dataset):
         y_path = os.path.join(basedir,"NoBackground")
         x_path = os.path.join(basedir,"WithBackground")
 
+        print(x_path)
+
         self.xs = []
         self.ys = []
-        for file in x_path: 
-            self.xs.append(cv2.imread(file))
 
-        for file in y_path: 
-            self.ys.append(cv2.imread(file,0))
+        for file in tqdm((os.listdir(x_path)[:2]),desc="Load inputs"): 
+            image = cv2.imread(os.path.join(x_path,file))
+            image = cv2.resize(image,(800,150))
+            image = np.rollaxis(image,2,0)
+            self.xs.append(image)
+
+        for file in tqdm((os.listdir(y_path)[:2]),desc="Load Labels"): 
+            image = cv2.imread(os.path.join(y_path,file))
+            image = cv2.resize(image,(800,150))
+            image = np.rollaxis(image,2,0)
+            self.ys.append(image)
+
+        if gpu:
+            self.dtype = torch.cuda.FloatTensor
+        else:
+            self.dtype = torch.FloatTensor
+
+        print(len(self.xs))
+        print(len(self.ys))
 
 
     def __getitem__(self, index):
-        return self.xs[index], self.ys[index]
+        return self.dtype(self.xs[index]), self.dtype(self.ys[index])
 
     def __len__(self):
         return len(self.xs)
--- train.py
+++ train.py
@@ -1,10 +1,11 @@
 import argparse
 import torch 
-import horovod.torch as hvd 
+#import horovod.torch as hvd 
 import numpy as np 
 from dataset import Mapdataset
 from models.unet import UNet
 from loss.loss import SSIM
+
 # define command line parameter 
 parser = argparse.ArgumentParser(description="Define the parameter of the training process")
 
@@ -14,33 +15,35 @@ parser.add_argument("--epochs", action="store", type=int)
 parser.add_argument("--batchsize",action="store",type=int)
 parser.add_argument("--basedir",action="store",type=str)
 parser.add_argument("--tbpath", action="store",type=str)
-parser.add_argument("--numTrainingSamples", action="store", type=int)
-parser.add_argument("--numValidationSamples", action="store", type=int)
-parser.add_argument("--numTestSamples", action="store", type=int)
 parser.add_argument("--gpu", action="store", type=int)
 parser.add_argument("--shuffle", action="store", type=int)
-parser.add_argument("--identifier",action="store", type=int)
+parser.add_argument("--identifier",action="store", type=str)
+
+
 
 args = parser.parse_args()
 
 trainDataset = Mapdataset(args.basedir, args.gpu)
-trainLoader = torch.utils.data.DataLoader(trainDataset,args.batch_size,args.shuffle,num_workers=2)
+trainLoader = torch.utils.data.DataLoader(trainDataset,args.batchsize,args.shuffle,num_workers=2)
 
-model = UNet(3,1)
+if args.gpu:
+    model = UNet(3,1).cuda()
+else:
+    model = UNet(3,1)
 
 optimizer = torch.optim.Adam(model.parameters(),lr=1e-3)
 
 ssim = SSIM()
-
+mse = torch.nn.MSELoss()
 # training loop 
 for epoch in range(args.epochs):
     for train_x, train_y in trainLoader:
             optimizer.zero_grad()
             prediction = model(train_x)
-            loss = -ssim(train_y, prediction)
+            loss = mse(prediction,train_y)
             loss.backward()
             optimizer.step()
-            if epoch % int(args.epoch / 10):
+            if epoch % 2:
                 print("Loss at Epoch",epoch+1,":",loss.item())
         
 
