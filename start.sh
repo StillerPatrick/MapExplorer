bc05c40ff9af568ef4398d83edffbd2760e8e7d4
Oliver Guhr <oliver.guhr@posteo.de>
2019-09-10 14:51:21 +0200

    new start params

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 train.py --epochs 10 --batchsize 32 --basedirtrain dataset/train/ --basedirvalidation dataset/Validation5k/ --basedirtest dataset/Test10k/ --tbpath runs --gpu 1 --shuffle 1 --identifier firstrun_mse
+python3 train.py --epochs 30 --batchsize 32 --basedirtrain dataset/train/ --basedirvalidation dataset/Validation5k/ --basedirtest dataset/Test10k/ --tbpath runs --gpu 1 --shuffle 1 --identifier firstrun_mse_10k
