77706a4634e669fb2fac5ffae8202e7e7f6105dc
Oliver Guhr <oliver.guhr@posteo.de>
2019-09-12 17:25:22 +0200

    dice loss added

--- loss/loss.py
+++ loss/loss.py
@@ -103,3 +103,22 @@ def msssim(img1, img2, window_size=11, size_average=True, val_range=None, normal
     # From Matlab implementation https://ece.uwaterloo.ca/~z70wang/research/iwssim/
     output = torch.prod(pow1[:-1] * pow2[-1])
     return output
+
+def dice_loss(pred, target):
+    """This definition generalize to real valued pred and target vector. 
+    This should be differentiable.
+    pred: tensor with first dimension as batch
+    target: tensor with first dimension as batch
+    """
+
+    smooth = 1.
+
+    # have to use contiguous since they may from a torch.view op
+    iflat = pred.contiguous().view(-1)
+    tflat = target.contiguous().view(-1)
+    intersection = (iflat * tflat).sum()
+
+    A_sum = torch.sum(tflat * iflat)
+    B_sum = torch.sum(tflat * tflat)
+    
+    return 1 - ((2. * intersection + smooth) / (A_sum + B_sum + smooth) )
