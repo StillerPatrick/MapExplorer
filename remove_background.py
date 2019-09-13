082973770fe46522eb7bdcc39cae1809a021fb30
Oliver Guhr <oliver.guhr@posteo.de>
2019-09-13 10:12:06 +0200

    test script for bg removal added

--- remove_background.py
+++ remove_background.py
@@ -24,8 +24,7 @@ def array_to_img(data,path):
 
 def clean_image(path,output_dir):
     image = Mapdataset.get_image(path)
-    output = model.forward(image.unsqueeze(0))
-    #output = 1 - output 
+    output =model.forward(image.unsqueeze(0))
     data =output[0,0].detach().numpy()
     out_path = output_dir + os.path.basename(path) + ".png"
     array_to_img(data,out_path)
@@ -35,7 +34,7 @@ def clean_image(path,output_dir):
     #plt.savefig(out_path)
 
 model = UNet(1,1)
-model.load_state_dict(torch.load("checkpoints/ckpt_20",map_location="cpu")["model"])
+model.load_state_dict(torch.load("checkpoints/data_56k_lr4_bin_dice2/ckpt_1",map_location="cpu")["model"])
 model.eval()
 folder = "docs/Real_map_data/"
 print("converting")
